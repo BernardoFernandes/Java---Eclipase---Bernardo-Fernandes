e986bc0faff6fdf673b4f078f0959f319aa8fd3e
BernardoFernandes <32074494+BernardoFernandes@users.noreply.github.com>
2017-11-24 14:58:24 +0000

    Add files via upload

---
+++ 5HojeéSextaFeira.java
@@ -0,0 +1,12 @@
+
+package javaapplication2;
+public class JavaApplication2 {
+
+
+    public static void main(String[] args) {
+        final String mensagem = ("Hoje é Sexta-Feira");
+   for (int I=1;I<=5;I++)
+   System.out.println(mensagem);
+    }
+    
+}
